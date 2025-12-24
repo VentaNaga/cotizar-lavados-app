c551cd3a7f722800b1177fcabc7af35b253009b8
VentaNaga <crymson512@gmail.com>
2025-12-24 00:04:35 -0300

    Agregado SVG transform y cambios de codigo

--- app/index.jsx
+++ app/index.jsx
@@ -1,37 +1,29 @@
 import React from 'react';
 import { Text, View, Image } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { FlatGrid } from 'react-native-super-grid';
-import icon from '../assets/image/icon.png';
 import { globalStyles, COLORS} from '../styles/globalStyles';
+import MapPin from '../assets/svg/map-pin.svg';
+import ChevronDown from '../assets/svg/chevron-down.svg';
 
 export default function HomeScreen() {
   const insets = useSafeAreaInsets();
-  const [items] = React.useState([
-    { name: 'Exterior' },
-    { name: 'Interior' },
-    { name: 'Express' },
-    { name: 'Detailing' },
-  ]);
-
   return (
-    <View style={{flex: 1, paddingTop: insets.top, backgroundColor: COLORS.bgDark}}>
-        <Image source={icon} style={globalStyles.logo} />
+    <View style={{flex: 1, paddingTop: insets.top, backgroundColor: COLORS.highlight}}>
+
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <MapPin width={24} height={24} color={COLORS.primary} style={{marginLeft: 10}} />
+          {/* Se requiere poder cambiar la ubicacion manualmente */}
+          {/*Se requiere que esta ubicacion sea dinamica*/}
+          <Text style={globalStyles.location}>Pudahuel, Santiago. </Text>
+          <ChevronDown width={24} height={24} color={COLORS.primary} style={{marginLeft: -5, marginTop: 8}} />
+        </View>
+        <View>
+          <Text style={globalStyles.text}>Mi Vehiculo:</Text>
+          
+        </View>
         <View>
             <Text style={globalStyles.title}>¿Qué es lo que estás buscando?</Text>
         </View>
-
-        <FlatGrid
-            itemDimension={130}
-            data={items}
-            style ={globalStyles.gridView}
-            spacing={10}
-            renderItem={({ item }) => (
-                <View style={[globalStyles.itemContainer, {backgroundColor: COLORS.bg}]}>
-                    <Text style={[globalStyles.itemName, {color: COLORS.text}]}>{item.name}</Text>
-                </View>
-            )}
-        />
     </View>
   );
 }
--- styles/globalStyles.jsx
+++ styles/globalStyles.jsx
@@ -1,16 +1,11 @@
 import { StyleSheet } from "react-native";
 
 export const COLORS = {
-    bgDark: 'hsl(0 0% 90%)',
-    bg: 'hsl(0 0% 95%)',
-    bgLight: 'hsl(0 0% 100%)',
-    text: 'hsl(0 0% 5%)',
-    textMuted: 'hsl(0 0% 30%)',
+    black: 'hsl(0 0% 0%)',
+    subtleDark: 'hsl(222 5% 40%)',
+    subtleLight: 'hsl(222 5% 70%)',
     highlight: 'hsl(235 100% 100%)',
-    border: 'hsl(236 9% 53%)',
-    borderMuted: 'hsl(236 13% 65%)',
-    primary: 'hsl(241 33% 37%)',
-    secondary: 'hsl(54 100% 12%)',
+    primary: 'hsl(244 89% 65%)',
     danger: 'hsl(9 21% 41%)',
     warning: 'hsl(52 23% 34%)',
     success: 'hsl(147 19% 36%)',
@@ -20,30 +15,29 @@ export const COLORS = {
 export const globalStyles = StyleSheet.create({
     title: {
         fontSize: 30,
-        fontWeight: 'bold',
         marginLeft: 20,
+        fontWeight: 'bold',
     },
-        logo: {
+    logo: {
         width: 100,
         height: 100,
         resizeMode: 'contain',
         marginLeft: 20,
     },
-    itemContainer: {
-        justifyContent: 'flex-center',
-        borderRadius: 5,
-        padding: 10,
-        height: 150,
-        boxShadow: '0px 12px 12px #00000015'
-    },
-    itemName: {
-        fontSize: 16,
-        color: '#fff',
-        fontWeight: '600',
-        textAlign: 'center',
-    },
     gridView: {
         marginTop: 10,
         flex: 1,
     },
+    location: {
+        marginTop: 30,
+        marginLeft: 5,
+        marginBottom: 25,
+        fontSize: 16,
+        color: COLORS.black,
+    },
+    text: {
+        fontSize: 16,
+        marginLeft: 39,
+        marginBottom: 10,
+    }
 });
